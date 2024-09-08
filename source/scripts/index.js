1c293094d29afc42750dd37ea41ed8e812d057ba
KleoSky <V-Kleo@yandex.ru>
2024-09-08 21:48:47 +0700

    Добавляет часть декоров

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,16 +1,16 @@
-let mainNav = document.querySelector('.main-navigation');
-let burger = document.querySelector('.button-burger');
+// const mainNav = document.querySelector('.main-navigation');
+// const burger = document.querySelector('.button-burger');
 
-burger.addEventListener('click', function () {
-  if (mainNav.classList.contains('main-navigation--closed')) {
-    mainNav.classList.remove('main-navigation--closed');
-    mainNav.classList.add('main-navigation--opened');
-    burger.classList.remove('button-burger--closed');
-    burger.classList.add('button-burger--opened');
-  } else {
-    mainNav.classList.add('main-navigation--closed');
-    mainNav.classList.remove('main-navigation--opened');
-    burger.classList.add('button-burger--closed');
-    burger.classList.remove('button-burger--opened');
-  }
-});
+// burger.addEventListener('click', function () {
+//   if (mainNav.classList.contains('main-navigation--closed')) {
+//     mainNav.classList.remove('main-navigation--closed');
+//     mainNav.classList.add('main-navigation--opened');
+//     burger.classList.remove('button-burger--closed');
+//     burger.classList.add('button-burger--opened');
+//   } else {
+//     mainNav.classList.add('main-navigation--closed');
+//     mainNav.classList.remove('main-navigation--opened');
+//     burger.classList.add('button-burger--closed');
+//     burger.classList.remove('button-burger--opened');
+//   }
+// });
