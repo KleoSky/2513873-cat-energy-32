1ddaf83260d3f2c5900f95e59c140a6e18dd34e5
KleoSky <V-Kleo@yandex.ru>
2024-09-08 21:25:34 +0700

    Добавляет часть декоров

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,16 @@
-/* в этот файл добавляет скрипты*/
+let mainNav = document.querySelector('.main-navigation');
+let burger = document.querySelector('.button-burger');
+
+burger.addEventListener('click', function () {
+  if (mainNav.classList.contains('main-navigation--closed')) {
+    mainNav.classList.remove('main-navigation--closed');
+    mainNav.classList.add('main-navigation--opened');
+    burger.classList.remove('button-burger--closed');
+    burger.classList.add('button-burger--opened');
+  } else {
+    mainNav.classList.add('main-navigation--closed');
+    mainNav.classList.remove('main-navigation--opened');
+    burger.classList.add('button-burger--closed');
+    burger.classList.remove('button-burger--opened');
+  }
+});
